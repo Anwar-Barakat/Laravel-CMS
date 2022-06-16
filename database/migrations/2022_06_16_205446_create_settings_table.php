3d298e0ebb3e34a96c06f614bf99852d826eeee9
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-17 00:33:02 +0300

    add settings table

---
+++ database/migrations/2022_06_16_205446_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('bio');
+            $table->text('facebook');
+            $table->text('telegram');
+            $table->text('email');
+            $table->text('github');
+            $table->text('copyright');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
