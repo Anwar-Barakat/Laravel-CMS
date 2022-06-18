6f1afc16b573bc4cdc136db0c3f7b4e802e2faf1
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-18 10:12:03 +0300

    add discuess comment plugin on frontend post page

--- resources/views/frontend/post.blade.php
+++ resources/views/frontend/post.blade.php
@@ -47,7 +47,7 @@
                     </div>
                     <div class="pt-5">
                         <h3 class="mb-5" id="dsq-count-scr">6 Comments</h3>
-                        <a href="">Comments</a>
+                        <a href="{{ route('frontend.tags.show', ['slug' => $post->slug]) }}#disqus_thread">Comments</a>
 
                         <div id="disqus_thread"></div>
                     </div>
@@ -163,4 +163,20 @@
             </div>
         </div>
     </div>
+@endsection
+
+
+@section('scripts')
+    <script>
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document,
+                s = d.createElement('script');
+            s.src = 'https://laravel-blog-tutorial-series.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <script id="dsq-count-scr" src="//laravel-blog-tutorial-series.disqus.com/count.js" async></script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+            Disqus.</a></noscript>
 @endsection
